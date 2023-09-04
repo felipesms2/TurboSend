3d765c08620dfcd8c4f4d4947badb9ef5ab7b9fd
felipesms2 <felipesms@live.com>
2023-09-04 23:45:00 +0000

    Creating subject

--- database/migrations/2023_09_04_232959_create_sends_table.php
+++ database/migrations/2023_09_04_232959_create_sends_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('sends', function (Blueprint $table) {
             $table->id();
+            $table->integer('subject_id');
+            $table->float('user_id');
             $table->timestamps();
         });
     }
